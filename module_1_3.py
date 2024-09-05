1fbf6a1a768d265ba349acc2c5422724699a084a
ferstapen <164868955+ferstapen@users.noreply.github.com>
2024-09-05 13:17:59 +0500

    Update module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -3,7 +3,7 @@ print('Name:', name)
 age = 38
 print('Age:', age)
 age = 38
-new_age = 39
-print('New Age:', new_age)
+age = 39
+print('New Age:', age)
 is_student = True
 print('Is Student:', is_student)
